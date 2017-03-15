4559fd7d48236a55ac185be1a16bcb7e535e5d99
marineco <marinecolonge@gmail.com>
2017-03-15 14:13:45 +0100

    ajout du render pour l'affichage de la liste

--- src/Items.js
+++ src/Items.js
@@ -24,7 +24,16 @@ class Items extends React.Component {
 			]
 		}
 	}
-
+	render() {
+		const listItems = this.state.items.map( item => {
+			return <li key={ item.id }>{ item.content }</li>;
+		});
+		return (
+			<div>
+				<ul>{ listItems }</ul>
+			</div>
+		);
+	}
 }
 
 export default Items
